0068b0e5a1a86ab8d05db2376a35266d0dae6703
ichi-09ryo <5t6w.jl@gmail.com>
2024-06-06 18:15:42 +0900

    一部一致、全て一致での表示変更

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,15 +1,37 @@
 class MapController < ApplicationController
   def index
-    @equipments = params[:equipments]
-    @selected_prefecture = params[:prefectures].first if params[:prefectures].present?
+    Rails.logger.debug "Received params: #{params.inspect}"
+
+    @equipments = params[:equipments].is_a?(String) ? params[:equipments].split(',') : Array(params[:equipments])
+    @selected_prefecture = params[:prefectures].is_a?(Array) ? params[:prefectures].first : params[:prefectures]
+    search_mode = params[:search_mode] || 'any'
+
+    Rails.logger.debug "Equipments: #{@equipments}, Selected Prefecture: #{@selected_prefecture}, Search Mode: #{search_mode}"
+
     equipment_ids = Equipment.where(equipment_name: @equipments).pluck(:id)
-    @gyms = Gym.joins(:gym_equipments).where(gym_equipments: { equipment_id: equipment_ids }).distinct
+
+    if search_mode == 'all'
+      # 全て一致の場合
+      @gyms = Gym.joins(:gym_equipments)
+                 .where(gym_equipments: { equipment_id: equipment_ids })
+                 .group('gyms.id')
+                 .having('COUNT(DISTINCT gym_equipments.equipment_id) = ?', @equipments.size)
+    else
+      # 一部一致の場合
+      @gyms = Gym.joins(:gym_equipments)
+                 .where(gym_equipments: { equipment_id: equipment_ids })
+                 .distinct
+    end
+
     @gyms = @gyms.where('address LIKE ?', "%#{@selected_prefecture}%") if @selected_prefecture.present?
 
     if @gyms.present?
       @map_center = { lat: @gyms.first.latitude, lng: @gyms.first.longitude }
     else
       @map_center = { lat: 35.6895, lng: 139.6917 } # デフォルトは東京
+      @gyms = []
     end
+
+    Rails.logger.debug "Gyms: #{@gyms.inspect}"
   end
 end
