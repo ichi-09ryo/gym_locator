86b6888a80a0468d28e8dc778673cd939c177f45
ichi-09ryo <5t6w.jl@gmail.com>
2024-06-07 15:17:45 +0900

    再検索フォーム、微修正

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,39 +1,44 @@
+# app/controllers/map_controller.rb
 class MapController < ApplicationController
   def index
     Rails.logger.debug "Received params: #{params.inspect}"
 
+    # パラメータが文字列の場合、配列に変換
     @equipments = params[:equipments].is_a?(String) ? params[:equipments].split(',') : Array(params[:equipments])
     @selected_prefecture = params[:prefectures].is_a?(Array) ? params[:prefectures].first : params[:prefectures]
 
     Rails.logger.debug "Equipments: #{@equipments}, Selected Prefecture: #{@selected_prefecture}"
 
-    equipment_ids = Equipment.where(equipment_name: @equipments).pluck(:id)
+    if @equipments.present? && @selected_prefecture.present?
+      equipment_ids = Equipment.where(equipment_name: @equipments).pluck(:id)
 
-    @gyms = Gym.joins(:gym_equipments)
-               .where(gym_equipments: { equipment_id: equipment_ids })
-               .distinct
-
-    @gyms = @gyms.where('address LIKE ?', "%#{@selected_prefecture}%") if @selected_prefecture.present?
+      @gyms = Gym.joins(:gym_equipments)
+                 .where(gym_equipments: { equipment_id: equipment_ids })
+                 .where('address LIKE ?', "%#{@selected_prefecture}%")
+                 .distinct
+    else
+      @gyms = Gym.none
+    end
 
     if @gyms.present?
       @map_center = { lat: @gyms.first.latitude, lng: @gyms.first.longitude }
     else
-      @map_center = { lat: 35.6895, lng: 139.6917 } # デフォルトは東京
+      @map_center = { lat: 35.6895, lng: 139.6917 } # 東京
       @gyms = []
     end
 
     Rails.logger.debug "Gyms: #{@gyms.inspect}"
 
-    # 定義された器具と地域を設定
+    # 定義された器具と都道府県
     @available_equipments = Equipment.select(:equipment_name).distinct.pluck(:equipment_name)
     @regions = {
-      "北海道・東北" => ["北海道", "青森", "岩手", "宮城", "秋田", "山形", "福島"],
-      "関東" => ["茨城", "栃木", "群馬", "埼玉", "千葉", "東京", "神奈川"],
-      "中部" => ["新潟", "富山", "石川", "福井", "山梨", "長野", "岐阜", "静岡", "愛知"],
-      "近畿" => ["三重", "滋賀", "京都", "大阪", "兵庫", "奈良", "和歌山"],
-      "中国" => ["鳥取", "島根", "岡山", "広島", "山口"],
-      "四国" => ["徳島", "香川", "愛媛", "高知"],
-      "九州・沖縄" => ["福岡", "佐賀", "長崎", "熊本", "大分", "宮崎", "鹿児島", "沖縄"]
+      "北海道・東北" => ["北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県"],
+      "関東" => ["茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県"],
+      "中部" => ["新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県", "静岡県", "愛知県"],
+      "近畿" => ["三重県", "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県"],
+      "中国" => ["鳥取県", "島根県", "岡山県", "広島県", "山口県"],
+      "四国" => ["徳島県", "香川県", "愛媛県", "高知県"],
+      "九州・沖縄" => ["福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"]
     }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'equipments', to: 'equipments#index'
   get 'map', to: 'map#index'
-  
+  get 'search', to: 'gyms#search'
   resources :map, only: [:index]
 end
